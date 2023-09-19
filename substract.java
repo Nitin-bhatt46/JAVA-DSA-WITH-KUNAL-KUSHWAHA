9940b486b53e6b28b1975f2b44d0b6470723b0d7
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2023-09-19 19:57:19 +0530

    Add files via upload

---
+++ substract.java
@@ -0,0 +1,9 @@
+public class substract {
+    public static void main(String args[]){
+        int a=10;
+        int b=8;
+        int c=a-b;
+        System.out.println("Subtraction of two digit "+a+" + "+b+" = "+c);
+    }
+    
+}
