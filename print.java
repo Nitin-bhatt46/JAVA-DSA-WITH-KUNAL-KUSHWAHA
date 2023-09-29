d1b394e4c91b92a1617dd6ea1a80e388120a738e
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2023-09-29 09:53:52 +0530

    Add files via upload

---
+++ print.java
@@ -0,0 +1,11 @@
+/*3. Print 6*3 in output, don’t print 18, just 6*3 should be in the
+output screen.
+*/
+import java.util.Scanner;
+public class print{
+    public static void main(String[] agrs)
+    {
+        
+        System.out.println("6*3");
+    }
+}
